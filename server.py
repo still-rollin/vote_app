4f4da2d1fbe17fd5a1aefce12a28ddb4be4ffb7e
Ayaan Siddiqui <ayaansiddiqui@Ayaans-MacBook-Air.local>
2024-01-30 00:07:56 +0530

    new commit message

--- server.py
+++ server.py
@@ -1,21 +1,33 @@
 #import flask module
 from flask import Flask,render_template, request
 from flask_mail import Mail, Message 
-
+votes={}
  
 # instance of flask application
 app = Flask(__name__)
  
 # home route that returns below text when root url is accessed
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-@app.route("/vote") 
+@app.route("/") 
 def random():
     return render_template('vote.html') 
-@app.route("/addvote")
-def addvote():
-    candidate=request.args.get('candidates')
+@app.route("/addposition")
+def add_position(position):
+    votes[position]={}
+def add_member(postion, candidate):
+    votes[position][candidate] = 0  
+@app.route("/voting")
+def vote(position, candidate):
+    votes[position][candidate] = votes[position][candidate] + 1
+
+
+    
+
+
+    
+    
+    
+    
+    
     
     
     

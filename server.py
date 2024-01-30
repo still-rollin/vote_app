516e945621fb81861f47b5806b0707c257097c86
Ayaan Siddiqui <ayaansiddiqui@Ayaans-MacBook-Air.local>
2024-01-30 14:12:30 +0530

    new commit message

--- server.py
+++ server.py
@@ -2,6 +2,11 @@
 from flask import Flask,render_template, request
 from flask_mail import Mail, Message 
 votes={}
+positionslist=[]
+candidateslist=[]
+prospectuslist=[]
+graduation_yearlist=[]
+all_candidates=[]
 
  
 # instance of flask application
@@ -11,16 +16,40 @@ app = Flask(__name__)
 @app.route("/") 
 def random():
     return render_template('vote.html') 
-@app.route("/addposition")
+@app.route("/add_candidates")
+def add():
+    position = request.args.get('position') 
+    positionslist.append(position)
+
+    candidate_name=request.args.get('candidate_name')
+    candidateslist.append(candidate_name)
+    prospectus=request.args.get('prospectus')
+    prospectuslist.append(prospectus)
+    graduation_year=request.args.get('graduationyear')
+    graduation_yearlist.append(graduation_year)
+  
+    details=(position,candidate_name,prospectus,graduation_year)
+   
+    
+    all_candidates.append(details)
+    print(all_candidates)
+    return render_template('index.html', details_list_in_html=all_candidates)
+
 def add_position(position):
     votes[position]={}
-def add_member(postion, candidate):
+def add_member(position, candidate):
     votes[position][candidate] = {}
     
     
 @app.route("/voting")
-def vote(position, candidate):
+def vote():
+    position = request.args.get('positions') 
+    candidate= request.args.get('candidates')
+    print(position)
+    print(candidate)
+    
     votes[position][candidate] = votes[position][candidate] + 1
+    return render_template('vote.html')
 
 
     

67578f9d18665d8b9efadcde9e20abc64b040fe1
Ayaan Siddiqui <ayaansiddiqui@Ayaans-MacBook-Air.local>
2024-02-02 17:12:46 +0530

    add results page

--- server.py
+++ server.py
@@ -17,7 +17,6 @@ class Candidate(db.Model):
     position=db.Column(db.String(100),nullable=False)
     graduation_year=db.Column(db.String(100),nullable=False)
     manifesto=db.Column(db.String(100),nullable=False)
-    
     def __repr__(self):
         return '<Candidate %r>' % self.name
 with app.app_context():
@@ -37,9 +36,7 @@ def get_candidates(position):
 def get_candidate_row(position):
     candidates= db.session.query(Candidate).filter(Candidate.position == position).distinct().all()
     return candidates
-
-    
-    
+   
 def create_candidate(name,votes,position,graduation_year,manifesto):
     existing_candidate = Candidate.query.filter_by(name=name, position=position).first()
 
@@ -112,8 +109,8 @@ def vote():
     candidate_to_vote=search_candidate(candidate_name,selected_position)
     candidate_to_vote.votes=candidate_to_vote.votes + 1
     bitsidlist.append(bitsid)
-    print('HELLO')
-    print(votes)
+    #print('HELLO')
+    #print(votes)
     status_message="Voted Successfully for  "+ candidate_name
     db.session.commit()
     return render_template('vote.html',status_message=status_message,positions=get_positions(),candidates=get_candidates(''))
